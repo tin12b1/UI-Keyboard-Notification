4432e882193a078540ebda7fce398b2479bea170
Tran Van Tin <nit@Trans-Mac.local>
2017-05-04 14:57:07 +0700

    Initial Commit

--- UIKeyboard Notification/ViewController.swift
+++ UIKeyboard Notification/ViewController.swift
@@ -12,6 +12,12 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let dismiss: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(ViewController.DismissKeyboard))
+        view.addGestureRecognizer(dismiss)
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(ViewController.keyboardWillShow), name:NSNotification.Name.UIKeyboardWillShow, object: self.view.window)
+        NotificationCenter.default.addObserver(self, selector: #selector(ViewController.keyboardWillHide), name:NSNotification.Name.UIKeyboardWillHide, object: self.view.window)
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -20,6 +26,33 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func DismissKeyboard(){
+        view.endEditing(true)
+    }
+    
+    func keyboardWillShow(sender: NSNotification) {
+        // 1
+        let userInfo = sender.userInfo!
+        // 2
+        let keyboardSize: CGSize = (userInfo[UIKeyboardFrameBeginUserInfoKey]! as AnyObject).cgRectValue.size
+        let offset: CGSize = (userInfo[UIKeyboardFrameEndUserInfoKey]! as AnyObject).cgRectValue.size
+        // 3
+        if keyboardSize.height == offset.height {
+            UIView.animate(withDuration: 0.1, animations: { () -> Void in
+                self.view.frame.origin.y -= keyboardSize.height
+            })
+        } else {
+            UIView.animate(withDuration: 0.1, animations: { () -> Void in
+                self.view.frame.origin.y += keyboardSize.height - offset.height
+            })
+        }
+    }
+    
+    func keyboardWillHide(sender: NSNotification) {
+        let userInfo = sender.userInfo!
+        let keyboardSize: CGSize = (userInfo[UIKeyboardFrameBeginUserInfoKey]! as AnyObject).cgRectValue.size
+        self.view.frame.origin.y += keyboardSize.height
+    }
 
 }
 
